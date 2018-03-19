bb5e0356430a4e85f7003df33cf138f62757cd5e
bstano <37197434+bstano@users.noreply.github.com>
2018-03-19 18:57:54 -0400

    Add files via upload

---
+++ Fibbo.py
@@ -0,0 +1,45 @@
+#This program takes a user's input and prints out a portion of the Fibbonacci Sequence
+#up to the user's specified length
+
+#Reads the user's input for the Sequence length
+#User is given 3 tries to enter valid length
+def user_length():
+    n = 0
+	print("You will get 3 tries to enter a valid integer. ")
+    while True:
+        user_length = input("What length should the sequence be?\n")
+		#make sure the user has entered a positive integer greater than or equal to 2
+        try: 
+            length = int(user_length)
+            if length >= 2:
+                return length
+            else:
+                print('The length must be a positive integer of 2 or more. Try again.')
+                n += 1
+        except:
+            print('The length should be a positive integer.')
+            n += 1
+        if n == 3:
+            print("You're having trouble, let's set the length to 20")
+            return 20
+    
+#Generates a Fibbonacci Sequence of a specified length   
+def create_seq(length):
+    result = []
+    for n in range(length):
+        if n == 0:
+            result.append(0)
+        elif n == 1:
+            result.append(1)
+        else:
+            result.append(result[n-1]+result[n-2])
+    return result
+    
+#Main function of the program
+def main():
+    length = user_length()
+    fibs = create_seq(length)
+    for fib in fibs:
+        print(fib)
+
+main()
